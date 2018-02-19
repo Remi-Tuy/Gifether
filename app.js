d05b0568b4f14e032c437e135d7bb09e0fac896a
Rémi Tuyaerts <30529561+Remi-Tuy@users.noreply.github.com>
2018-02-19 23:46:33 +0100

    Custom boilerplate.

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path')
-  , bodyParser = require('body-parser')
-  , favicon = require('serve-favicon')
-  , logger = require('morgan')
-  , methodOverride = require('method-override');
+var index = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(favicon(__dirname + '/public/images/favicon.png'));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
-app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-if (app.get('env') == 'development') {
-	app.locals.pretty = true;
-}
+app.use('/', index);
+app.use('/users', users);
 
-app.get('/', routes.index);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
